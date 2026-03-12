bcb963e14e7745d7199d1eac7dcfc2af95822741
Perfecto <perfectogardoce@gmail.com>
2026-03-12 19:54:06 +0800

    updated the view/blade

--- routes-controller-rev/resources/views/06-dynamic-array-route.blade.php
+++ routes-controller-rev/resources/views/06-dynamic-array-route.blade.php
@@ -1,5 +1,6 @@
 <div>
-    <h1>Students Page</h1>
+    <h1>Dynamic Array Page</h1>
+    <h3>Students Page</h3>
     <p>Section: {{ $section }}</p>
     <ul>
         @foreach ($students as $student)
